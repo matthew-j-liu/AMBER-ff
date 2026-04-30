745d96890084d9c4acf0b021485073cdc41c6b2e
Matthew Liu <matthewliu@berkeley.edu>
2026-04-30 13:22:19 -0700

    identify atom types by AMBER classification (hydrocarbon only)

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "read_xyz.hpp"
 #include "ff_params.hpp"
 #include "read_params.hpp"
+#include "atom_typing.hpp"
 #include <iostream>
 #include <stdexcept>
 
@@ -19,7 +20,8 @@ int main(int argc, char** argv)
 
     // parse molecule (first argument in CLI)
     MoleculeGraph mol = read_xyz(argv[1]);
-    std::cout << "Molecule loaded!" << std::endl;
+    assign_amber_types(mol);
+    std::cout << "Molecule loaded!" << std::endl << std::endl;
     std::cout << "atoms: " << mol.num_atoms() << std::endl;
     std::cout << "bonds: " << mol.num_bonds() << std::endl << std::endl;
 
@@ -29,15 +31,16 @@ int main(int argc, char** argv)
     std::cout << "# of bond params: " << ff.bonds.size() << std::endl;
     std::cout << "# of angle params: " << ff.angles.size() << std::endl;
     std::cout << "# of dihedral params: " << ff.dihedrals.size() << std::endl;
-    std::cout << "# of vdw params: " << ff.vdw.size() << std::endl;
+    std::cout << "# of vdw params: " << ff.vdw.size() << std::endl << std::endl;
 
     for (int i = 0; i < mol.num_atoms(); i++)
     {
         const Atom& a = mol.atoms[i];
 
         std::cout << i << "  " << a.element << "  (" << a.position(0)
-                  << ", " << a.position(1) << ", " << a.position(2) << ") | "
-                  << "  neighbors={";
+                  << ", " << a.position(1) << ", " << a.position(2) << ")" 
+                  << " | AMBER type: " << a.amber_type << " | "
+                  << "neighbors={";
 
         std::vector<int> neighbors = mol.neighbors(i);
 
